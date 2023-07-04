71186672d10797cbfb1eb850a512b501723fe459
Moh Yusuf <muhamadyusuf03112001@gmail.com>
2023-07-04 22:34:14 +0700

    Add link to register and fix focus outline in register form

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,23 +10,33 @@ import Container from 'react-bootstrap/Container';
 function Header() {
   return (
     <>
-      <Navbar  expand="lg" className="navbar">
-          <Container> 
-          <Navbar.Brand href="/"><img src={Logo} alt="Logo" height={"40px"}/></Navbar.Brand>
+      <Navbar expand="lg" className="navbar">
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={Logo} alt="Logo" height={"40px"} />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="Menu">Menu</Nav.Link>
-            <Nav.Link href="About">About</Nav.Link>
-            <Nav.Link href="Contact">Contact</Nav.Link>
-          
-          </Nav>
-          
-          <div className="navLogin">
-            <a href='Login' className='text-decoration-none text-dark mx-3 fw-semibold'>Login</a>
-            <Button variant="dark" className='navButton'>Get Started</Button>
-          </div>
-          </Navbar.Collapse>    
+            <Nav className="me-auto">
+              <Nav.Link href="Menu">Menu</Nav.Link>
+              <Nav.Link href="About">About</Nav.Link>
+              <Nav.Link href="Contact">Contact</Nav.Link>
+            </Nav>
+
+            <div className="navLogin">
+              <a
+                href="Login"
+                className="text-decoration-none text-dark fw-semibold mx-3"
+              >
+                Login
+              </a>
+              <a href="Register">
+                <Button variant="dark" className="navButton">
+                  Get Started
+                </Button>
+              </a>
+            </div>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -6,52 +6,65 @@ import { Button } from 'react-bootstrap';
 import Img from '../image/registerImg.jpg'
 
 function Register(){
-    return(
-        <>
-         <Container className='loginContainer'>
-            <Row>
-                <Col lg={6} md={6} sm={12}>
-                    <div className='registerForm'>
-                        <h1>Register, and Start Sharing Love</h1>
-                        <Form>
-                        <Form.Group className="mb-3 formInput" >
-                                <Form.Label>Full Name</Form.Label>
-                                <Form.Control type="text"  />
-                            </Form.Group>
-                            <Form.Group className="mb-3 formInput" >
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email"  />
-                            </Form.Group>
-                            <Form.Group className="mb-3 formInput" >
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password"  />
-                            </Form.Group>
-                            <Form.Group className="mb-3 formInput" >
-                            <Form.Label>Register As</Form.Label>
-                            <Form.Select aria-label="Default select example" className='option'>
-                                <option></option>
-                                <option value="1">Member</option>
-                                <option value="2">Donor</option>
-                                <option value="3">Patner</option>
-                                <option value="4">Driver</option>
-                                <option value="5">Volunteer</option>
-                            </Form.Select>
-                            </Form.Group>
-                        </Form>
-                        <div className='loginButtonWrapper'>
-                        <Button variant='dark' className='loginButton'>Register</Button>
-                        </div>
-                    </div>
-                </Col>
-                <Col lg={6} md={6} sm={12}>
-                    <div className='registerImg'>
-                    <img src={Img} alt="Logo" />
-                    </div>
-                    
-                </Col>
-            </Row>
+    return (
+      <>
+        <Container className="loginContainer">
+          <Row>
+            <Col lg={6} md={6} sm={12}>
+              <div className="registerForm">
+                <h1>Register, and Start Sharing Love</h1>
+                <Form className='px-4'>
+                  <Form.Group className="formInput mb-3">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control
+                      style={{ outline: "none", boxShadow: "none" }}
+                      type="text"
+                    />
+                  </Form.Group>
+                  <Form.Group className="formInput mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      style={{ outline: "none", boxShadow: "none" }}
+                      type="email"
+                    />
+                  </Form.Group>
+                  <Form.Group className="formInput mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      style={{ outline: "none", boxShadow: "none" }}
+                      type="password"
+                    />
+                  </Form.Group>
+                  <Form.Group className="formInput mb-3">
+                    <Form.Label>Register As</Form.Label>
+                    <Form.Select
+                      aria-label="Default select example"
+                      className="option"
+                    >
+                      <option></option>
+                      <option value="1">Member</option>
+                      <option value="2">Donor</option>
+                      <option value="3">Patner</option>
+                      <option value="4">Driver</option>
+                      <option value="5">Volunteer</option>
+                    </Form.Select>
+                  </Form.Group>
+                </Form>
+                <div className="loginButtonWrapper">
+                  <Button variant="dark" className="loginButton">
+                    Register
+                  </Button>
+                </div>
+              </div>
+            </Col>
+            <Col lg={6} md={6} sm={12}>
+              <div className="registerImg">
+                <img src={Img} alt="Logo" />
+              </div>
+            </Col>
+          </Row>
         </Container>
-        </>
+      </>
     );
 }
 export default Register;
